d982e85d76b7a4c11e5c0a7fa0f1f69692f93e47
shahidmonowarr <shahidmonowar21@gmail.com>
2023-07-16 10:01:34 +0600

    design improved

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,31 +1,23 @@
-
-import { Link } from 'react-router-dom';
-import { IBook } from '../types/globalTypes';
-import { Button } from './ui/button';
+import { Link } from "react-router-dom";
+import { IBook } from "../types/globalTypes";
+import { Button } from "./ui/button";
 
 interface IProps {
   book: IBook;
 }
 
 export default function bookCard({ book }: IProps) {
-
   return (
     <div>
       <div className="rounded-2xl h-[480px] flex flex-col items-start justify-between p-5 overflow-hidden shadow-md border border-gray-100 hover:shadow-2xl hover:scale-[102%] transition-all gap-2">
-      <Link to={`/book-details/${book._id}`} className="w-full">
-          <img className='h-52 w-auto' src={book?.image_link} alt="book" />
+        <Link to={`/book-details/${book._id}`} className="w-full">
+          <img className="h-52 w-auto" src={book?.image_link} alt="book" />
           <h1 className="text-xl font-semibold">{book?.title}</h1>
         </Link>
-        <p className="text-sm">
-          Author: {book?.author}
-        </p>
-        <p className="text-sm">
-          Genre: {book?.genre}
-        </p>
+        <p className="text-sm">Author: {book?.author}</p>
+        <p className="text-sm">Genre: {book?.genre}</p>
         <p className="text-sm">Publication Date: {book?.publicationDate}</p>
-        <Button variant="default" >
-          Add to wishlist
-        </Button>
+        <Button variant="default">Add to wishlist</Button>
       </div>
     </div>
   );
--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,20 +1,33 @@
-import BookCard from '../components/BookCard';
-import { useGetBooksQuery } from '../redux/api/apiSlice';
-import { IBook } from '../types/globalTypes';
-
+import { FiSearch } from "react-icons/fi";
+import BookCard from "../components/BookCard";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
+import { useGetBooksQuery } from "../redux/api/apiSlice";
+import { IBook } from "../types/globalTypes";
 
 export default function Books() {
   const { data } = useGetBooksQuery(undefined);
   //const { toast } = useToast();
 
   return (
-    <div className="grid grid-cols-12 max-w-7xl mx-auto relative ">
-      <div className="col-span-3 z mr-10 space-y-5 border rounded-2xl border-gray-200/80 p-5 self-start sticky top-16 h-[calc(100vh-80px)]">
+    <div className="max-w-7xl mx-auto relative ">
+      <div className="max-w-2xl mx-auto mt-5 mb-5">
+        <form className="flex gap-5 items-center">
+          <Input className="min-h-[30px]" placeholder="Search for books" />
+          <Button
+            type="submit"
+            className="rounded-full h-10 w-10 p-2 text-[25px]"
+          >
+            <FiSearch />
+          </Button>
+        </form>
       </div>
-      <div className="col-span-9 grid grid-cols-3 gap-10 pb-20">
-        {data?.data?.map((book: IBook) => (
-          <BookCard key={book._id} book={book} />
-        ))}
+      <div className="grid grid-cols-12 max-w-7xl mx-auto relative ">
+        <div className="col-span-12 grid grid-cols-4 gap-10 pb-20">
+          {data?.data?.map((book: IBook) => (
+            <BookCard key={book._id} book={book} />
+          ))}
+        </div>
       </div>
     </div>
   );
